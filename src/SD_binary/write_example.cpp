19b8188fd169b62ec0c10cdd8791fa95d354101a
John M. Franck (SU laptop) <jmfranck@syr.edu>
2023-02-02 15:54:30 -0500

    split open and write across different methods -- doesn't work!

--- src/SD_binary/write_example.cpp
+++ src/SD_binary/write_example.cpp
@@ -5,22 +5,24 @@
 #define SD_CHIP_SELECT_PIN 4
 
 File myFile;
+int loopcounter;
+int datalen = 10;
 
 //A structure which two elements
 struct Datastore {
-  // note that the types on the arduino are slightly different, and you should
-  // make sure that you match the type of the data that you're trying to
-  // store!!
-  unsigned int Voltage; // this should just be a raw arduino reading DO NOT DO THE OD MATH ON THE ARDUINO!!!
-  long int time; // similarly, just the result of millis
+    // note that the types on the arduino are slightly different, and you should
+    // make sure that you match the type of the data that you're trying to
+    // store!!
+    unsigned int Voltage; // this should just be a raw arduino reading DO NOT DO THE OD MATH ON THE ARDUINO!!!
+    long int time; // similarly, just the result of millis
 };
 
 const char *filename="test.dat";
 void setup()
 {
-    int j;
-    int datalen = 10;
-    int num_written;
+
+
+
 
     // Open serial communications and wait for port to open:
     Serial.begin(9600);
@@ -40,24 +42,31 @@ void setup()
         SD.remove(filename);
     }
     myFile = SD.open(filename, FILE_WRITE);
-    Datastore mydata[datalen];
-    for(j=0;j<datalen;j++){
-        mydata[j].Voltage = j*10; // generate fake voltage data that's just 0, 10, 20... 90
-        mydata[j].time = j + 1000; // generate fake time data that's 0.1, 1.1, 2.1, etc.
-                                      //printf("wrote %d %f\n",mydata[j].Voltage, mydata[j].time);
-    }
-    // {{{ in this block, the arduino commands will be similar, but somewhat different!
-    num_written = myFile.write((const uint8_t *)mydata, sizeof(mydata));
-    myFile.close();
-    Serial.println("wrote data");
-    Serial.println("use this to det struct size");
-    Serial.println(sizeof(mydata));
-    Serial.println("this is how much I wrote");
-    Serial.println(num_written);
-    // }}}
+    loopcounter = 0;
 }
 
-void loop(){}
+void loop(){
+    int j;
+    int num_written;
+    Datastore mydata[datalen];
+    if(loopcounter==0){
+        for(j=0;j<datalen;j++){
+            mydata[j].Voltage = j*10; // generate fake voltage data that's just 0, 10, 20... 90
+            mydata[j].time = j + 1000; // generate fake time data that's 0.1, 1.1, 2.1, etc.
+                                       //printf("wrote %d %f\n",mydata[j].Voltage, mydata[j].time);
+        }
+        // {{{ in this block, the arduino commands will be similar, but somewhat different!
+        num_written = myFile.write((const uint8_t *)mydata, sizeof(mydata));
+        myFile.close();
+        Serial.println("wrote data");
+        Serial.println("use this to det struct size");
+        Serial.println(sizeof(mydata));
+        Serial.println("this is how much I wrote");
+        Serial.println(num_written);
+    }
+    // }}}
+    loopcounter += 1;
+}
 
 
 
