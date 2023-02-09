6c5d635340685ee828a5366b7ade1e082b5c86a9
Eli Paster <113472035+eipaster1@users.noreply.github.com>
2023-02-09 07:26:39 -0500

    Will commit when structure is print my myself

--- src/DataSD/DataSDDraft.cpp
+++ src/DataSD/DataSDDraft.cpp
@@ -1,104 +1,68 @@
-/* Importing Libraries required to use SD and Arduino Funcitons*/
-#include <SD.h>
-#include <SPI.h>
+//Working copy inspired by web
 #include <Arduino.h>
-#define SD_CHIP_SELECT_PIN 4
+#include <SPI.h>
+#include <SD.h>
 
-/*Introducing variables for analyzing the analog input*/
 
-//OD Placehold for single analog read 
-int OD;
-//Analog Tag for OD
-int ReadOD = A0;
-//Amount of Readings before taking an average
-int numReadings;
-//Analog number recorded onto the SD
-int ODRecord;
-//Readings Counter Goal
-int numReadTar =5;
-//Readings Counter
-int numRead;
-//File indentifier
-File myfile;
-
-//Making a structure with the needed variables
+//defining a structure
 struct Datastore {
-    // note that the types on the arduino are slightly different, and you should
-    // make sure that you match the type of the data that you're trying to
-    // store!!
-    unsigned int AnalogOD; // this should just be a raw arduino reading DO NOT DO THE OD MATH ON THE ARDUINO!!!
-    unsigned long int time; // similarly, just the result of millis
-    
+    unsigned int Analog;
+    unsigned long int time
 };
 
+// I am going to test the ability to print structure to terminal first
 
-//Now making a specific data type from the prevously defined structure
+//Now setting up the analog variables
 
-Datastore logchunk[1];
+int phold; // The counter for the elements in struct
+int strsize = 5; // The size of the structure 
+
+
+int rcount; // The reading counter
+unsigned long int acount; // How the readings are added and eventuall divided by rcount
+int amcount = 5; //The amount of reading taken before average
+
+
+
 
 void setup(){
-/*Setting pins*/
+//Setting serial and pin 
 pinMode(A0,INPUT);
-digitalWrite(10,HIGH);
-//Making name of file
-//why not const char here
-char *filename = "SD.Logger.dat";
-
-//Now making file opener
-
-myfile = SD.open(filename,FILE_WRITE);
- 
-//Starting serial terminal and making average control
-
 Serial.begin(9600);
-ODRecord = 0;
-numRead = 0;
-OD = 0;
+
+phold = 0;
+rcount = 0;
+acount = 0;
+
+//Set the counting values to zero
+
+
+
+
 
 }
+
+
 
 void loop(){
-//After Recording Value onto SD the num Reader goes back to zero
-digitalWrite(10,HIGH);
-OD = analogRead(ReadOD);
-ODRecord += OD;
-numRead += 1;
 
-//Now using if statement to determine if the number of readings were met
+acount =+ analogRead(A0); //Reading the voltage 
+rcount += 1; //adding to the Voltage counter to probe the average
 
-if(numRead == numReadTar){
-ODRecord = ODRecord/numRead;
-Serial.print("Analog OD Value: ");
-//Recording Data point to serial Reader
+if(rcount == amcount){
 
-Serial.print(ODRecord);
-Serial.print(" At time: ");
-Serial.print(millis()/500);
-Serial.println(" Seconds");
 
-//Recording OD variable within the strucure datalog
 
-logchunk[1].AnalogOD = ODRecord;
-logchunk[1].time = millis()/1000;
-
-//Once the data is written to the structure the SD commands will write to file
-
-myfile.write((uint8_t *)logchunk, sizeof(1));
-
-//Will now close the file to make sure data is saved
-
-myfile.close();
-
-//Resetting the Counters and the Recordering Variable
-
-numRead = 0;
-ODRecord = 0;
-
-}
-
-delay(100);
 
 }
 
 
 
+
+
+
+
+
+
+
+}
