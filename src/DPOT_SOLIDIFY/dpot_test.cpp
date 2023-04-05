9b97dceae4831adcb66e13b1e221e02a422e2c72
Eli Paster <113472035+eipaster1@users.noreply.github.com>
2023-04-05 16:34:09 -0400

    The D_pot test is successful no more mods

--- src/DPOT_SOLIDIFY/dpot_test.cpp
+++ src/DPOT_SOLIDIFY/dpot_test.cpp
@@ -1,15 +1,29 @@
 #include <Arduino.h>
 #include <SPI.h>
+/*
+[platformio]
+src_dir = src/DPOT_SOLIDIFY
+
+[env:uno]
+platform = atmelavr
+board = uno
+framework = arduino
+lib_deps = 
+	arduino-libraries/SD@^1.2.4
+	paulstoffregen/OneWire@^2.3.7
+	milesburton/DallasTemperature@^3.11.0
+*/
+
 
 
 // Setting integers to for data keepers
 
- const int num_average = 10; //The number of counts before the average is taken
- const int sensing_pin_op_amp = A0; //Where the analog integer will be housed
- const int slave_select_digi = 10; // Slave selected pin for the arduino (can change depending on board)
-const int serial_speed = 9600;
-//Defining the SPI SETTINGS
-SPISettings mySetting(16000000, MSBFIRST,SPI_MODE0 ); 
+ int num_average = 10; //The number of counts before the average is taken
+ int sensing_pin_op_amp = A0; //Where the analog integer will be housed
+ int slave_select_digi = 10; // Slave selected pin for the arduino (can change depending on board)
+int serial_speed = 9600;
+
+SPISettings mySetting(16000000, MSBFIRST,SPI_MODE0 ); //Defining the SPI SETTINGS
 // the defult settings for the arduino (Mega may be different)
 // Initialized settings for the ATMEGA328P core
 
@@ -22,34 +36,30 @@ SPI.transfer(digi_value);
 digitalWrite(slave_select_digi,HIGH); // Switching recieving bit back to zero
 SPI.endTransaction();  // Ends the transaction for this specific spi device
 }
-double take_average_opamp(){
-  int store = 0; // Store the amount of analog reading it has taken
-  
-  for(int i;i<num_average; i++){
-    store += analogRead(sensing_pin_op_amp); // iterates adds the value from the sensor from the op amp
-  }
-  return store/num_average; //Return the divided out num averge to get average
-}
+
 
 void setup(){
-SPI.begin();// Initializes the SPI bus by setting SCK, MOSI, and SS to outputs, pulling SCK and MOSI low, and SS high.
-Serial.begin(serial_speed);
-pinMode(sensing_pin_op_amp,INPUT);
 
+SPI.begin();// Initializes the SPI bus by setting SCK, MOSI, and SS to outputs, pulling SCK and MOSI low, and SS high.
+pinMode(slave_select_digi,OUTPUT);
+pinMode(sensing_pin_op_amp,INPUT);
+Serial.begin(9600);
+digiwrite(0);
+delay(5000);
 
 }
 
 void loop(){
-int j = 0; // Restarting the variable resistor program
-  for( int j;j<=254;j++){
+int j = 100; // Restarting the variable resistor program
+  for( j;j>=0;j--){
     Serial.print("At Digi Pot STEP");
     Serial.print(j);
-    Serial.print(": With a the sensor reading");
-    Serial.print(int(take_average_opamp));  //Taking the average of the opamp function rounded to integeger which is not what much accuracy taken away
+    digiwrite(j); //Write a new digiwrite resister value
+    Serial.print(": With a the sensor reading: ");
+    Serial.print(analogRead(A0)); //Taking the average of the opamp function rounded to integeger which is not what much accuracy taken away
+    Serial.print("\n") ; 
     delay(500); //Delay to make sure voltimeter can confirm voltage changes happending
     
   }
-
-
-
+  
 }
