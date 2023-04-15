79b9f18530fc85b602fdfa2a7c06f2b56012b8d6
Eli Paster <113472035+eipaster1@users.noreply.github.com>
2023-04-15 19:17:46 -0400

    This successfully writes to the CARD

--- src/SD/SD-Card-Testing.cpp
+++ src/SD/SD-Card-Testing.cpp
@@ -3,8 +3,24 @@
 
 File myFile;
 
+
+struct datastore {
+  int data1;
+  int data2;
+  int data3;
+
+};
+
+datastore buffer[10]; // Have created the data store buffer information
+
+
+
+
+
+
 void setup() {
   // Open serial communications and wait for port to open:
+  int i = 0;
   pinMode(10,OUTPUT);
   digitalWrite(10,HIGH);
   Serial.begin(9600);
@@ -21,26 +37,36 @@ void setup() {
   }
   Serial.println("initialization done.");
 
+char *filename = "text.dat";
+for(i; i<10;i++){
+
+  buffer[i].data1 = i*10;
+  buffer[i].data2 = i*20;
+  buffer[i].data3 = i*30;
+}
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
-  myFile = SD.open("test.txt", FILE_WRITE);
+  myFile = SD.open(filename, FILE_WRITE);
 
   // if the file opened okay, write to it:
   if (myFile) {
     Serial.print("Writing to test.txt...");
-    myFile.println("testing 1, 2, 3, 4.");
+    int num = myFile.write((const uint8_t *)buffer, sizeof(buffer));
+
+    Serial.print("Num_written ");
+    Serial.println(num);
     // close the file:
     myFile.close();
-    Serial.println("done.");
+  
   } else {
     // if the file didn't open, print an error:
     Serial.println("error opening test.txt");
   }
 
   // re-open the file for reading:
-  myFile = SD.open("test.txt");
+  myFile = SD.open("test.dat");
   if (myFile) {
-    Serial.println("test.txt:");
+    Serial.println("test.dat:");
 
     // read from the file until there's nothing else in it:
     while (myFile.available()) {
