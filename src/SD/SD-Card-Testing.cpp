7c247c8288a08005cea30fb6e1a9491dc09b6697
Eli Paster <113472035+eipaster1@users.noreply.github.com>
2023-04-15 19:33:48 -0400

    First part of the structure written

--- src/SD/SD-Card-Testing.cpp
+++ src/SD/SD-Card-Testing.cpp
@@ -20,7 +20,7 @@ datastore buffer[10]; // Have created the data store buffer information
 
 void setup() {
   // Open serial communications and wait for port to open:
-  int i = 0;
+
   pinMode(10,OUTPUT);
   digitalWrite(10,HIGH);
   Serial.begin(9600);
@@ -38,12 +38,7 @@ void setup() {
   Serial.println("initialization done.");
 
 char *filename = "text.dat";
-for(i; i<10;i++){
 
-  buffer[i].data1 = i*10;
-  buffer[i].data2 = i*20;
-  buffer[i].data3 = i*30;
-}
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
   myFile = SD.open(filename, FILE_WRITE);
@@ -51,6 +46,7 @@ for(i; i<10;i++){
   // if the file opened okay, write to it:
   if (myFile) {
     Serial.print("Writing to test.txt...");
+    myFile.seek(EOF);
     int num = myFile.write((const uint8_t *)buffer, sizeof(buffer));
 
     Serial.print("Num_written ");
@@ -58,28 +54,26 @@ for(i; i<10;i++){
     // close the file:
     myFile.close();
   
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
   }
 
-  // re-open the file for reading:
-  myFile = SD.open("test.dat");
-  if (myFile) {
-    Serial.println("test.dat:");
 
-    // read from the file until there's nothing else in it:
-    while (myFile.available()) {
-      Serial.write(myFile.read());
-    }
-    // close the file:
-    myFile.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening test.txt");
-  }
 }
 
 void loop() {
-  // nothing happens after setup
+int i = 0;
+for(i; i<10;i++){
+
+  buffer[i].data1 = i*10;
+  buffer[i].data2 = i*20;
+  buffer[i].data3 = i*30;
+}
+   
+    int num = myFile.write((const uint8_t *)buffer, sizeof(buffer));
+ myFile.seek(EOF);
+    Serial.print("Num_written ");
+    Serial.println(num);
+    // close the file:
+    myFile.close();
+
+    delay(500);
 }
