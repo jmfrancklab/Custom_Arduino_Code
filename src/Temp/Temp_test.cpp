a98d856e7660b98e4e1ff693ed78cdd273539c49
Eli Paster <113472035+eipaster1@users.noreply.github.com>
2023-04-17 17:30:31 -0400

    * Found Temp Digi Addresses *Sensor 1 : 0x28, 0x26, 0x3B, 0x94, 0x97, 0x0A, 0x03, 0x56 Sensor 2 : 0x28, 0x0B, 0xB3, 0x79, 0x97, 0x00, 0x03, 0x6C

--- src/Temp/Temp_test.cpp
+++ src/Temp/Temp_test.cpp
@@ -1,82 +1,61 @@
-/*
- * Rui Santos
- * Complete Project Details https://randomnerdtutorials.com
-*/
-
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// Data wire is plugged into port 4 on the Arduino
+// Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 3
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+
+void printAddress(DeviceAddress deviceAddress)
+{ 
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    Serial.print("0x");
+    if (deviceAddress[i] < 0x10) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+    if (i < 7) Serial.print(", ");
+  }
+  Serial.println("");
+}
+
+
+
+// Setup a oneWire instance to communicate with any OneWire devices
 OneWire oneWire(ONE_WIRE_BUS);
 
-// Pass our oneWire reference to Dallas Temperature. 
+// Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
-int numberOfDevices; // Number of temperature devices found
+// variable to hold device addresses
+DeviceAddress Thermometer;
 
-DeviceAddress tempDeviceAddress; // We'll use this variable to store a found device address
-// function to print a device address
-void printAddress(DeviceAddress deviceAddress) {
-  for (uint8_t i = 0; i < 8; i++) {
-    if (deviceAddress[i] < 16) Serial.print("0");
-      Serial.print(deviceAddress[i], HEX);
-  }
-}
-void setup(void) {
+int deviceCount = 0;
+
+void setup()
+{
   // start serial port
   Serial.begin(9600);
-  
+
   // Start up the library
   sensors.begin();
-  
-  // Grab a count of devices on the wire
-  numberOfDevices = sensors.getDeviceCount();
-  
+
   // locate devices on the bus
-  Serial.print("Locating devices...");
+  Serial.println("Locating devices...");
   Serial.print("Found ");
-  Serial.print(numberOfDevices, DEC);
+  deviceCount = sensors.getDeviceCount();
+  Serial.print(deviceCount, DEC);
   Serial.println(" devices.");
-
-  // Loop through each device, print out address
-  for(int i=0;i<numberOfDevices; i++) {
-    // Search the wire for address
-    if(sensors.getAddress(tempDeviceAddress, i)) {
-      Serial.print("Found device ");
-      Serial.print(i, DEC);
-      Serial.print(" with address: ");
-      printAddress(tempDeviceAddress);
-      Serial.println();
-		} else {
-		  Serial.print("Found ghost device at ");
-		  Serial.print(i, DEC);
-		  Serial.print(" but could not detect address. Check power and cabling");
-		}
-  }
-}
-
-void loop(void) { 
-  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("");
   
-  // Loop through each device, print out temperature data
-  for(int i=0;i<numberOfDevices; i++) {
-    // Search the wire for address
-    if(sensors.getAddress(tempDeviceAddress, i)){
-		
-		// Output the device ID
-		Serial.print("Temperature for device: ");
-		Serial.println(i,DEC);
-
-    // Print the data
-    float tempC = sensors.getTempC(tempDeviceAddress);
-    Serial.print("Temp C: ");
-    Serial.print(tempC);
-    Serial.print(" Temp F: ");
-    Serial.println(DallasTemperature::toFahrenheit(tempC)); // Converts tempC to Fahrenheit
-    } 	
+  Serial.println("Printing addresses...");
+  for (int i = 0;  i < deviceCount;  i++)
+  {
+    Serial.print("Sensor ");
+    Serial.print(i+1);
+    Serial.print(" : ");
+    sensors.getAddress(Thermometer, i);
+    printAddress(Thermometer);
   }
-  delay(5000);
 }
+
+void loop()
+{}
 
