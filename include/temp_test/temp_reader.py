654a55a6a8cc315c85f18482e6a0fb4e1df20e29
Eli Paster <113472035+eipaster1@users.noreply.github.com>
2023-04-18 15:54:42 -0400

    Successful Task Think Done Commit Temp

--- include/temp_test/temp_reader.py
+++ include/temp_test/temp_reader.py
@@ -2,28 +2,22 @@ from pylab import *
 
 
 
-mytype = dtype([    ("T_1","<f4"),    ("T_2","<f4"),    ("Average_of_Ts","<f4"),    ("millistime","<u4")])
+mytype = dtype([    ("T_Water","<f4"),    ("T_average_of_Al_Block","<f4"),  ("millistime","<u4")])
 
 result = fromfile("Temp_Rec.DAT", dtype=mytype)
 
-plot(result["millistime"], result["T_1"], "o")
+plot(result["millistime"], result["T_Water"], "o")
 
 xlabel('Time [mms]')
-ylabel('Temperature 1 [C]')
+ylabel('Temperature of Water [C]')
 title('Temperature 1 Reading vs Time')
 figure()
 
-plot(result["millistime"], result["T_2"],"o")
+plot(result["millistime"], result["T_average_of_Al_Block"],"o")
 xlabel("Time [mms]")
-ylabel("Temperature 2 [C]")
-title("Temperature 2 Reading vs Time")
+ylabel(" Metal Temperature [C]")
+title("Metal Temperature Reading vs Time")
 
-figure()
-
-plot(result["millistime"], result["Average_of_Ts"],"o")
-title("Average Reading Temperature vs Time")
-xlabel("Time [mms]")
-ylabel("Average Temp [C]")
 
 
 
