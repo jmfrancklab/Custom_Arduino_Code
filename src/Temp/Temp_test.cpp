fb9b752a12a1d3928a369c953c298139659a05d8
Eli Paster <113472035+eipaster1@users.noreply.github.com>
2023-04-18 13:41:11 -0400

    First Draft for Rough Task

--- src/Temp/Temp_test.cpp
+++ src/Temp/Temp_test.cpp
@@ -2,46 +2,35 @@
 #include <DallasTemperature.h>
 #include <SPI.h>
 #include <SD.h>
-//Test
+
+
 
 // Data wire is plugged into digital pin 2 on the Arduino
 #define ONE_WIRE_BUS 2
-
 // Setup a oneWire instance to communicate with any OneWire device
 OneWire oneWire(ONE_WIRE_BUS);	
-
 // Pass oneWire reference to DallasTemperature library
 DallasTemperature sensors(&oneWire);
+const uint8_t sensorA[8] = {0x28, 0x26, 0x3B, 0x94, 0x97, 0x0A, 0x03, 0x56};
+const uint8_t sensorB[8] = {0x28, 0x0B, 0xB3, 0x79, 0x97, 0x00, 0x03, 0x6C};
+const uint8_t sensorC[8] = {0x28, 0x10, 0x0A, 0x94, 0x97, 0x0A, 0x03, 0x40};
+//0x28, 0x26, 0x3B, 0x94, 0x97, 0x0A, 0x03, 0x56 is A
+//0x28, 0x0B, 0xB3, 0x79, 0x97, 0x00, 0x03, 0x6C is B
+//0x28, 0x10, 0x0A, 0x94, 0x97, 0x0A, 0x03, 0x40 is C
 
+int deviceCount = 0; // The devicecount checker or how many temp recorders are there
 
-
-// Creating a structure for the temperature recording...
+// Creating a structure for the temperature recording... and FILE SET UP STUFF
 
 struct temprecord { 
-  float T_1;
-  float T_2; 
-  float Average_of_Ts;
+  float T_Water;
+  float T_average_of_Al_Block; 
   unsigned long int millistime;
-
 };
-
 File tempfile; //Created an instance of the open file
 const int datalen = 20; 
-temprecord buffer[datalen];
-              // Size off each struct or... amount of time it takes to dump data to SD NOTE MUST BE CONST INT
+temprecord buffer[datalen]; // Size off each struct or... amount of time it takes to dump data to SD NOTE MUST BE CONST INT
 const char *filename = "Temp_Rec.DAT"; // The name of the file IMPORTANT CANNOT HAVE  more than 8 character within the name must follow the file naming system FAT 32
-
-void temp_rec(int j){
-
-sensors.requestTemperatures();
-buffer[j].T_1 = sensors.getTempCByIndex(0);
-buffer[j].T_2 = sensors.getTempCByIndex(1);
-buffer[j].Average_of_Ts = (sensors.getTempCByIndex(0) +  sensors.getTempCByIndex(1))/2;
-buffer[j].millistime = millis();
-
-
-}
-
 void datadump()
 
 {
@@ -85,23 +74,103 @@ void checkSD()
    
   
 }
-int deviceCount = 0;
 
 
-void setup(void)
+
+//Temperature Setup stuff for writing to SD card as well as printing to serial and relay activator variables;
+float ttar = 35;
+float tolorance = 4;
+
+
+float t_max = ttar + tolorance; // The tolorances for each constraint
+float t_min = ttar - tolorance; // The tolorances for each constraint
+
+// The relay activators; and heat controllers
+int activator1 = 7;
+int activator2 = 6;
+bool heater_on = false; // Heater starts of not on
+bool hit_max = false;   // Has not entered max of min yet
+bool hit_min = false;
+
+//The two functions below one checks the function for the correct range within the temp and the other checks or recording
+void temp_rec(int j){
+
+sensors.requestTemperatures();
+buffer[j].T_Water = sensors.getTempC(sensorA);
+buffer[j].T_average_of_Al_Block= (sensors.getTempC(sensorB)+sensors.getTempC(sensorC))/2;
+buffer[j].millistime = millis();
+
+
+}
+void temp_stabilizer()
 {
+    if (sensors.getTempC(sensorA) >= t_max && !hit_max)
+    {
+        digitalWrite(activator1, LOW);
+        digitalWrite(activator2, LOW);
+        hit_max = true;
+        hit_min = false;
+        Serial.println("Heater Off");
 
+    }
+    else if (sensors.getTempC(sensorA) <= t_min && !hit_min)
+    {
+        digitalWrite(activator1, HIGH);
+        digitalWrite(activator2, HIGH);
+        hit_min = true;
+        hit_max = false;
+        Serial.println("Heater On");
+    }
+  }
+void temp_report(){
+  sensors.requestTemperatures();
+  Serial.print("Sensor A: ");
+  Serial.print(sensors.getTempC(sensorA));
+  Serial.print((char)176);//shows degrees character
+  Serial.print(" C ,");
+  Serial.print("Sensor B: ");
+  Serial.print(sensors.getTempC(sensorB));
+  Serial.print((char)176);//shows degrees character
+  Serial.print(" C ,");
+  Serial.print(" Sensor C: ");
+   Serial.print(sensors.getTempC(sensorC));
+  Serial.print((char)176);//shows degrees character
+  Serial.print(" C ");
+
+
+
+}
+//Makes the steps to ready up the sensor
+void initalize_t_and_relay (){
   sensors.begin();	// Start up the library
-  Serial.begin(9600);
-  checkSD();
-  
-  // locate devices on the bus
   Serial.print("Locating devices...");
   Serial.print("Found ");
   deviceCount = sensors.getDeviceCount();
   Serial.print(deviceCount, DEC);
   Serial.println(" devices.");
   Serial.println("");
+  pinMode(activator1,OUTPUT);
+  pinMode(activator2,OUTPUT);
+  digitalWrite(activator1,LOW);
+  digitalWrite(activator2,LOW);
+
+
+
+}
+
+
+
+
+
+
+
+void setup(void)
+{
+  Serial.begin(9600);
+  checkSD();
+  initalize_t_and_relay();
+  
+  
 }
 
 
@@ -115,6 +184,9 @@ void loop(void)
   delay(200);
 
 datadump();
+temp_stabilizer();
+temp_report();
+
 delay(1000);
 
 
