5cdad35958d5398e224aeceb50f22da8364aeeff
Eli Paster <113472035+eipaster1@users.noreply.github.com>
2023-04-19 11:16:07 -0400

    Added heater state to on vs off

--- include/temp_test/temp_reader.py
+++ include/temp_test/temp_reader.py
@@ -3,7 +3,7 @@ from scipy.interpolate import UnivariateSpline
 from numpy import dtype
 
 # Define data type and read data
-mytype = dtype([("T_Water", "<f4"), ("T_average_of_Al_Block", "<f4"), ("millistime", "<u4")])
+mytype = dtype([("T_Water", "<f4"), ("T_average_of_Al_Block", "<f4"), ("millistime", "<u4"),("heater_state","<u4")])
 result = fromfile("Temp_Rec.DAT", dtype=mytype)
 
 # Convert millisecond time to seconds
@@ -17,7 +17,7 @@ tck_T_average = UnivariateSpline(time_sec, result["T_average_of_Al_Block"])
 new_time = linspace(time_sec.min(), time_sec.max(), 1000)
 
 # Create a figure with 3 subplots
-fig, axs = subplots(3, 1, figsize=(10, 10))
+fig, axs = subplots(4, 1, figsize=(10, 10))
 
 # Set common x and y limits for all subplots
 xlim([0, 6000])
@@ -30,7 +30,7 @@ axs[1].plot(new_time, tck_T_average(new_time), label="T_average_of_Al_Block")
 # Plot temperature readings vs time
 axs[2].plot(result["millistime"] / 1000, result["T_Water"], "--", label="T_Water")
 axs[2].plot(result["millistime"] / 1000, result["T_average_of_Al_Block"], "--", label="T_average_of_Al_Block")
-
+axs[3].plot(result["millistime"]/1000,result["heater_state"],"o" , label = "Heater State 0 = off, 1 = on")
 # Add labels and legend
 axs[0].set_ylabel('Temperature [C]')
 axs[1].set_ylabel('Temperature [C]')
@@ -38,9 +38,14 @@ axs[2].set_xlabel('Time [s]')
 axs[2].set_ylabel('Temperature [C]')
 axs[0].set_title('Spline fits of temperature vs time')
 axs[2].set_title('Temperature readings vs time')
+axs[3].set_title("Heater state vs Time")
+axs[3].set_xlabel( "Time [s]")
+axs[3].set_ylabel("On or Off")
+
 axs[0].legend()
 axs[1].legend()
 axs[2].legend()
+axs[3].legend()
 
 # Show plot
 show()
