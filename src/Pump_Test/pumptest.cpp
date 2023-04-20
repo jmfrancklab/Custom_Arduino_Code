b501e945795922f3dbce9cc34a5d9ea4e2132f63
Eli Paster <113472035+eipaster1@users.noreply.github.com>
2023-04-20 18:49:34 -0400

    Try to foce the interupt to finish failed

--- src/Pump_Test/pumptest.cpp
+++ src/Pump_Test/pumptest.cpp
@@ -12,13 +12,19 @@ volatile bool TempFlag = false;
 volatile bool StatusFlag = false;
 volatile bool Pump_switch = false;
 bool Pump_State = false;
+int push_pump;
+float push_temp;
+bool inter_finished = false;
+
 
 int decider; //Helps with deciding which side program to activate
 
 //Defining the handling interupt flag 
 
 void handleInterrupt(){
-    Serial.print("Choose a Mode");
+   while(!inter_finished){
+    Serial.flush();
+    Serial.println("Choose a Mode");
     Serial.println("Type 1 for Status Update");
     Serial.println("Type 2 for Pump Control");
     Serial.println("Type 3 for Temp Control");
@@ -27,48 +33,49 @@ void handleInterrupt(){
     int decider = Serial.parseInt();
 
     switch(decider) {
-  case 1:
+case 1:
   StatusFlag = true;
   Serial.println("Wait for status update: ");
     break;
   case 2:
    PumpFlag = true;
    Serial.println("Wait for Pump Setting: ");
+   Serial.println("Please type the pump control: (0-255)");
+   push_pump = Serial.parseInt();
+
     break;
-  case 3:
+case 3:
     TempFlag = true;
     Serial.println("Wait for TempSetting");
+    Serial.println("Please type temp setting (20 C < T < 50 C )");
+    push_temp = Serial.parseFloat();
     break;
-    case 4:
+case 4:
     Serial.println("Switching Pump State");
+    Serial.println("Will Put in the xor state soon");
+
+
 
 
   default:
     break;
-}
-
 
 
 }
+inter_finished = true;
+   }
 
 
-
-
-
-
-
-
-
-
-
-
+}
 
 void setup(){
-
-
-
-
+Serial.begin(9600);
+pinMode(IRupt, INPUT_PULLUP);
+   attachInterrupt(digitalPinToInterrupt(IRupt), handleInterrupt, FALLING);
 }
+
+
+
 
 
 void loop (){ 
