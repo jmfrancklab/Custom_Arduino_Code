321f9eff72b388da50ef258493d0d64320d60446
Eli Paster <113472035+eipaster1@users.noreply.github.com>
2023-04-21 14:27:29 -0400

    Pump User Code Complete

--- src/Pump_Test/pumptest.cpp
+++ src/Pump_Test/pumptest.cpp
@@ -12,7 +12,7 @@ volatile bool TempFlag = false;
 volatile bool StatusFlag = false;
 volatile bool Pump_switch = false;
 int push_pump;
-bool Pump_State = false;
+bool Pump_State = true;
 float push_temp;
 bool inter_finished = false;
 bool inter_on = false;
@@ -28,12 +28,11 @@ inter_on = true;
 
 }
 void user_choice_interface () {
-  Serial.println("Choose a Mode");
-  Serial.println("Type 1 for Status Update");
-  Serial.println("Type 2 for Pump Control");
-  Serial.println("Type 3 for Temp Control");
-  Serial.println("Type 4 for Turning on or off the Pump");
-  Serial.println("Type 5 for Pump Direction Switch");
+  Serial.println("\n\n\n\n\nChoose a Mode");
+  Serial.println("Type 1 for Pump Control");
+  Serial.println("Type 2 for Temp Control");
+  Serial.println("Type 3 for Turning on or off the Pump");
+  Serial.println("Type 4 for Pump Direction Switch");
 
   while (!Serial.available()) {
     delay(100); // Wait for input
@@ -41,27 +40,25 @@ void user_choice_interface () {
   int decider = Serial.parseInt();
   
   switch(decider) {
+    Serial.flush(); // To make sure if the user presses a faulty key the program does not fail
+    
     case 1:
       
-      Serial.println("Wait for status update: ");
-      Serial.println("Should have printed the status update");
-      break;
-    case 2:
-      
       Serial.println("Wait for Pump Setting: ");
-      Serial.println("Please type the pump control: (0-255), Type xxx or xx. for <100");
+      Serial.println("Please type the pump control: (0-255), Type xxx or xx. for x < 100");
+      Serial.println("Make sure pump is on first");
       
       while (Serial.available()<3 || Serial.peek() == ".") {
         delay(100); // Wait for input
       }
       push_pump = Serial.parseInt();
       analogWrite(ENA_MotorPin,push_pump);
-      Serial.println("Pump set to analog setting of ");
+      Serial.print("Pump set to analog setting of: ");
       Serial.print(push_pump);
     
       
       break;
-    case 3:
+    case 2:
       
       Serial.println("Wait for TempSetting");
       Serial.println("Please type temp setting (20 C < T < 50 C ) Type xx.xx ");
@@ -69,8 +66,11 @@ void user_choice_interface () {
         delay(100); // Wait for input
       }
       push_temp = Serial.parseFloat();
+      Serial.print("Set temp baseline to: ");
+      Serial.print(push_temp);
+      Serial.print(" C");
       break;
-    case 4:
+    case 3:
       Serial.println("Switching Pump State");
       Pump_State ^= true; 
       //Switching the value from its orgional if off then on if on then off
@@ -82,28 +82,30 @@ void user_choice_interface () {
       } else if (Pump_State ==false){
           digitalWrite(IN1,HIGH);
           digitalWrite(IN2, LOW);
-          analogWrite(ENA_MotorPin,30);
+          analogWrite(ENA_MotorPin,10);
           
 
       }
       break;
-      case 5:
+      case 4:
       if ( digitalRead(IN1) == 1 && digitalRead(IN2) == 0){
 
         digitalWrite(IN1,LOW);
         digitalWrite(IN2, HIGH);
+        Serial.println("Switch Motor Flow Direction");
 
       } else if (digitalRead(IN1) == 0 && digitalRead(IN2) == 1){
 
         digitalWrite(IN1,HIGH);
         digitalWrite(IN2,LOW);
-
+        Serial.print("Switched Motor Flow Direction");
       } else {
 
         Serial.print("Should Turn on Pump First");
         
       }
-
+      
+      break;
 
     default:
       
@@ -125,7 +127,6 @@ attachInterrupt(digitalPinToInterrupt(IRupt), handleInterrupt, FALLING);
 
 
 void loop (){ 
-Serial.println("Doing other stuff");
 delay(500);
 if(inter_on){
     user_choice_interface();
