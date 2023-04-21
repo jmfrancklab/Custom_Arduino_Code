7216ac0161c5702c18ea22ac61c5dcda5a891741
Eli Paster <113472035+eipaster1@users.noreply.github.com>
2023-04-21 13:08:54 -0400

    Full Pump Code Test draft complete will test

--- src/Pump_Test/pumptest.cpp
+++ src/Pump_Test/pumptest.cpp
@@ -11,8 +11,8 @@ volatile bool PumpFlag = false;
 volatile bool TempFlag = false;
 volatile bool StatusFlag = false;
 volatile bool Pump_switch = false;
-bool Pump_State = false;
 int push_pump;
+bool Pump_State = false;
 float push_temp;
 bool inter_finished = false;
 bool inter_on = false;
@@ -33,6 +33,7 @@ void user_choice_interface () {
   Serial.println("Type 2 for Pump Control");
   Serial.println("Type 3 for Temp Control");
   Serial.println("Type 4 for Turning on or off the Pump");
+  Serial.println("Type 5 for Pump Direction Switch");
 
   while (!Serial.available()) {
     delay(100); // Wait for input
@@ -41,11 +42,12 @@ void user_choice_interface () {
   
   switch(decider) {
     case 1:
-      StatusFlag = true;
+      
       Serial.println("Wait for status update: ");
+      Serial.println("Should have printed the status update");
       break;
     case 2:
-      PumpFlag = true;
+      
       Serial.println("Wait for Pump Setting: ");
       Serial.println("Please type the pump control: (0-255)");
       
@@ -53,11 +55,14 @@ void user_choice_interface () {
         delay(100); // Wait for input
       }
       push_pump = Serial.parseInt();
+      analogWrite(ENA_MotorPin,push_pump);
+      Serial.println("Pump set to analog setting of ");
+      Serial.print(push_pump);
     
       
       break;
     case 3:
-      TempFlag = true;
+      
       Serial.println("Wait for TempSetting");
       Serial.println("Please type temp setting (20 C < T < 50 C ) Type [xx.xx] ");
       while (Serial.available()< 4|| Serial.peek() == "."){
@@ -67,22 +72,44 @@ void user_choice_interface () {
       break;
     case 4:
       Serial.println("Switching Pump State");
-      Serial.println("Will Put in the xor state soon"); 
-      Pump_State ^ true; // If true then false if false then true
-      if( Pump_State ==true){
-        analogWrite(ENA_MotorPin, 0);
+      Pump_State ^= true; 
+      //Switching the value from its orgional if off then on if on then off
+      if( Pump_State == true){
+        digitalWrite(IN1,LOW);
+        digitalWrite(IN2,LOW);
 
 
+      } else if (Pump_State ==false){
+          digitalWrite(IN1,HIGH);
+          digitalWrite(IN2, LOW);
+          analogWrite(ENA_MotorPin,30);
+          
+
+      }
+      break;
+      case 5:
+      if ( digitalRead(IN1) == 1 && digitalRead(IN2) == 0){
+
+        digitalWrite(IN1,LOW);
+        digitalWrite(IN2, HIGH);
+
+      } else if (digitalRead(IN1) == 0 && digitalRead(IN2) == 1){
+
+        digitalWrite(IN1,HIGH);
+        digitalWrite(IN2,LOW);
+
+      } else {
+
+        Serial.print("Should Turn on Pump First");
+        
       }
 
 
-
-      break;
     default:
+      
       break;  
   }
 } 
-void pump_turn 
 
 void setup(){
 Serial.begin(9600);
