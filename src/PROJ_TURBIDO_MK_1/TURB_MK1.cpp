3ee2b51c0447bc20adc4a214f81d503876c4af00
Eli Paster <113472035+eipaster1@users.noreply.github.com>
2023-04-21 15:17:58 -0400

    Continuing Progress Towards writing the script

--- src/PROJ_TURBIDO_MK_1/TURB_MK1.cpp
+++ src/PROJ_TURBIDO_MK_1/TURB_MK1.cpp
@@ -4,28 +4,55 @@
 #include <SPI.h>
 #include <SD.h>
 #include <OneWire.h>
+#include <DallasTemperature.h>
+
 
 //Now defining important pins and instances hardwired
+
+//TEMP PINS
 
 #define ONE_WIRE_BUS A3 //The busline for the three temperature sensors
 #define activator1 7 //The Relay pin for Bang Control
 #define activator2 6 //The second relay control switch for Bang Control
+
+//OPTICS AND SD
+
 #define sensing_pin_op_amp A0 //The reading for cell count corrolation
 #define slave_select_digi 9 // The slave communicator for the Digipot
+#define Chip_Select_Pin 4 // The ethernet and SD chip select pin
+
+//PUMP CONTROL
+
+#define ENA_MotorPin 5 //Pin for PWM SPEED of PUMP MODULATION
+#define IN1 A4 //Logical Determiner for on and off
+#define IN2 A1 //Logical Determiner for on and off
+
+//Button
+
+#define IRupt 2 // The pin which opens up the menu for user interfacing
 
 
+//DEFINING STRUCURES INSTENCES AND ADDRESSES
 
-OneWire oneWire(ONE_WIRE_BUS);	
-// Pass oneWire reference to DallasTemperature library
-DallasTemperature sensors(&oneWire);
+//Temp Control
 
+OneWire oneWire(ONE_WIRE_BUS); // Pass oneWire reference to DallasTemperature library
+DallasTemperature sensors(&oneWire); // Pass oneWire reference to DallasTemperature library
 const uint8_t sensorA[8] = {0x28, 0x26, 0x3B, 0x94, 0x97, 0x0A, 0x03, 0x56};
 const uint8_t sensorB[8] = {0x28, 0x0B, 0xB3, 0x79, 0x97, 0x00, 0x03, 0x6C};
 const uint8_t sensorC[8] = {0x28, 0x10, 0x0A, 0x94, 0x97, 0x0A, 0x03, 0x40};
-
 //0x28, 0x26, 0x3B, 0x94, 0x97, 0x0A, 0x03, 0x56 is A
 //0x28, 0x0B, 0xB3, 0x79, 0x97, 0x00, 0x03, 0x6C is B
 //0x28, 0x10, 0x0A, 0x94, 0x97, 0x0A, 0x03, 0x40 is C
+
+
+
+//SPI and SD Settings and Control
+
+SPISettings mySetting(16000000, MSBFIRST, SPI_MODE0); // Defining the SPI SETTINGS
+
+
+
 
 
 
