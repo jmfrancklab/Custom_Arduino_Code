5a2ca9af680d085a6f40588687eee524fd7219ac
Eli Paster <113472035+eipaster1@users.noreply.github.com>
2023-05-02 08:18:24 -0400

    Graph figure sorted out

--- read_write_example/plot_elements.py
+++ read_write_example/plot_elements.py
@@ -1,7 +1,5 @@
 from pylab import *
 
-
-
 filename = "DATAREC.dat"
 
 mytype = dtype([("millistime","<u4"), 
@@ -14,40 +12,42 @@ mytype = dtype([("millistime","<u4"),
                 ("heater_state", "<u4")])
 
 result = fromfile(filename, dtype=mytype)
-for i in range(size(result)):
-    print(result[i])
 
-plot(result["millistime"], result["digi_pot_wiper_position"],"--")
-xlabel("Time [mms]")
-ylabel("PWO [0-100]")
-title("Time vs. PWO Position")
+fig, axs = subplots(2, 3, figsize=(18, 8))
 
-figure()
+axs[0, 0].plot(result["millistime"], result["digi_pot_wiper_position"],"--", label="PWO")
+axs[0, 0].set_xlabel("Time [mms]")
+axs[0, 0].set_ylabel("PWO [0-100]")
+axs[0, 0].set_title("Time vs. PWO Position")
+axs[0, 0].legend()
 
-plot(result["millistime"], result["Voltage_analog_input"],"--")
-title("Analog Integer Reading vs. Time")
-xlabel("Time [mms]")
-ylabel("Analog [0-1024]")
+axs[0, 1].plot(result["millistime"], result["Voltage_analog_input"],"--", label="Analog")
+axs[0, 1].set_xlabel("Time [mms]")
+axs[0, 1].set_ylabel("Analog [0-1024]")
+axs[0, 1].set_title("Analog Integer Reading vs. Time")
+axs[0, 1].legend()
 
-figure()
-plot(result["millistime"], result["T_average_of_Al_Block"], "--")
-plot(result["millistime"], result["T_Water"], "--")
+axs[0, 2].plot(result["millistime"],result["heater_state"],"--", label="Heater")
+axs[0, 2].set_xlabel("Time [mms]")
+axs[0, 2].set_ylabel("Heater on/off")
+axs[0, 2].set_title("Heater State vs Time")
+axs[0, 2].legend()
 
-xlabel('Time [mms]')
-ylabel('Temperature [C]')
-title('Temp vs. Time (avg)')
+axs[1, 0].plot(result["millistime"], result["T_average_of_Al_Block"], "--", label="Average")
+axs[1, 0].plot(result["millistime"], result["T_Water"], "--", label="Water")
+axs[1, 0].plot(result["millistime"],result["temp_baseline"], label="Baseline")
+axs[1, 0].set_xlabel('Time [mms]')
+axs[1, 0].set_ylabel('Temperature [C]')
+axs[1, 0].set_title('Temp vs. Time (avg)')
+axs[1, 0].legend()
 
-figure()
+axs[1, 1].plot(result["millistime"], result["pump_speed_setting"], "--", label="Pump Speed")
+axs[1, 1].set_xlabel("Time [mms]")
+axs[1, 1].set_ylabel("pump speed 0-255")
+axs[1, 1].set_title("Time vs. Pump Speed")
+axs[1, 1].legend()
 
+axs[1, 2].axis('off')  # Add an empty subplot to keep the layout
 
-plot(result["millistime"], result["pump_speed_setting"], "--")
-
-xlabel("Time [mms]")
-ylabel("pump speed 0-255")
-title("Time vs. Pump Speed")
-
-
-
-
-
+fig.tight_layout()
 show()
