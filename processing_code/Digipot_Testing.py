13f6fa7164063b71dc0e6bc5c5f9c15d4869c322
Eli Paster <113472035+eipaster1@users.noreply.github.com>
2023-05-06 13:51:44 -0400

    Data File Naming

--- processing_code/Digipot_Testing.py
+++ processing_code/Digipot_Testing.py
@@ -1,7 +1,8 @@
 from pylab import *
 
-filename = "DATAREC.dat"
-
+filename = "Saftey_Resistence_Run.DAT"
+filename1 = "Eli_Circ_2k.DAT"
+filename2 = "20OHM_Saftey.DAT"
 mytype = dtype([
     ("millistime", "<u4"),
     ("Voltage_analog_input", "<f4"),
@@ -15,22 +16,22 @@ mytype = dtype([
     ("DigiV","<u4"),
 ])
 
-result = fromfile(filename, dtype=mytype)
+result = fromfile(filename2, dtype=mytype)
 
 # Shift millis to start at 0
 result["millistime"] = result["millistime"] - result["millistime"][0]
 
 fig, axs = subplots(1, 2, figsize=(18, 8))
 
-axs[0].plot(5000 - 5000/(result["digi_pot_wiper_position"]), (result["Voltage_analog_input"])*5/1024, ".", label="IRLED Voltage")
-axs[0].plot(5000 - 5000/(result["digi_pot_wiper_position"]), (result["DigiV"])*5/1024, ".", label="Digi Voltage")
-axs[0].set_xlabel("Resistence OHMS of Digi POT")
+axs[0].plot((result["digi_pot_wiper_position"]), (result["Voltage_analog_input"])*5/1024, ".", label="IRLED Voltage")
+axs[0].plot((result["digi_pot_wiper_position"]), (result["DigiV"])*5/1024, ".", label="Digi Voltage")
+axs[0].set_xlabel("Step Value of POT")
 axs[0].set_ylabel("Voltage")
 axs[0].set_title("Response of Voltage Const T and Stepping DigiPot")
 axs[0].legend()
 
-axs[1].plot(5000 - 5000/(result["digi_pot_wiper_position"]), (result["T_average_of_Al_Block"]), ".")
-axs[1].set_xlabel("Resistence of DigiPot")
+axs[1].plot((result["digi_pot_wiper_position"]), (result["T_average_of_Al_Block"]), ".")
+axs[1].set_xlabel("Step Value of Pot")
 axs[1].set_ylabel("Metal Temperature")
 axs[1].set_title("Metal Temperature Response")
 axs[1].legend()
